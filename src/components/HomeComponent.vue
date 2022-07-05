a5e196561fdab002311a2976bb1541b9c2765e02
pasan04 <pasankamburugamuwa1@gmail.com>
2022-07-05 20:41:55 +0530

    Navigating-with-router-link

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>HOME</h1>
+    <router-link to="/about"> My About component </router-link>
     <p>This is home page.</p>
   </div>
 </template>
@@ -25,5 +26,13 @@ li {
 }
 a {
   color: #42b983;
+}
+
+a:hover,
+a:active,
+a.router-link-a {
+  color: #42b983;
+  border-color: #42b983;
+  background-color: blue;
 }
 </style>
